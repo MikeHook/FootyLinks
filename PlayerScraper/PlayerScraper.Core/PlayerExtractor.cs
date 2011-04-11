22326729f7cb4c7625ee7aa8bd87825f1dd22242
MikeHook <hook.mike@gmail.com>
2011-04-11 08:50:59 +0100

    - Added method to save scrapped HTML to file. - Updated GetPlayerName method to extract the player name from the HTML.

--- PlayerScraper/PlayerScraper.Core/PlayerExtractor.cs
+++ PlayerScraper/PlayerScraper.Core/PlayerExtractor.cs
@@ -17,10 +17,11 @@ namespace PlayerScraper.Core
 
 		public string GetPlayerName()
 		{
-			var playerNode = _htmlDocument.DocumentNode.SelectSingleNode("//a[@href");
-			var playerNameNode = playerNode.SelectSingleNode("");
-
-			return "";
+			//Retrieve the player name from the 'clubInfo' table
+			var playerTableNode = _htmlDocument.DocumentNode.SelectSingleNode("//table[@class='clubInfo']");
+			var playerTdNodes = playerTableNode.Descendants("td");
+			var playerName = playerTdNodes.First().InnerText.Trim();
+			return playerName;
 		}
 	}
 }
