9ed2ace7e4be4dc59ed0581f9edd9851713e28c7
MikeHook <hook.mike@gmail.com>
2011-04-12 08:15:46 +0100

    Moved scraping logic in HtmlScraper class.

--- PlayerScraper/PlayerScraper.App/Program.cs
+++ PlayerScraper/PlayerScraper.App/Program.cs
@@ -14,39 +14,15 @@ namespace PlayerScraper.App
 	{
 		static void Main(string[] args)
 		{
+
+			
+			/*
 			HtmlDocument doc = new HtmlDocument();
-
-			int playerId = 31854;
-			string filePath = string.Format(@"C:\_Development\Android\FootyLinks\PlayerScraper\PlayerScraper.App\ScrappedHtml\{0}", playerId);
-			//writeStreamToFile(filePath, getHtmlStream("http://www.soccerbase.com/players/player.sd?player_id=", playerId));
-
 			doc.Load(filePath);
 
 			var playerExtractor = new PlayerExtractor(doc);
 			var playerName = playerExtractor.GetPlayerName();
-		}
-
-		public static Stream getHtmlStream(string pageToScrape, int playerId)
-		{
-			string httpAddress = string.Format("{0}{1}",pageToScrape, playerId); 
-
-			WebRequest objRequest = System.Net.HttpWebRequest.Create(httpAddress);
-			WebResponse objResponse = objRequest.GetResponse();
-
-			return objResponse.GetResponseStream();
-		}
-
-
-		public static void writeStreamToFile(string filePath, Stream stream)
-		{			
-			using (var streamReader = new StreamReader(stream))
-			{
-				string fileContents = streamReader.ReadToEnd();
-				//Remove the carriage return, new lines
-				fileContents = Regex.Replace(fileContents, "[\t\r\n]", " ");
-
-				File.WriteAllText(filePath, fileContents);
-			}
-		}
+			 */ 
+		}		
 	}
 }
